5dc3bd567554bc8bacfcec6feb827ca701151a91
AnthonyMarc23 <anthony@adaptivelite.com>
2018-05-10 00:16:06 -0400

    javascript framework

---
+++ script.js
@@ -0,0 +1,64 @@
+"use strict";
+// TODO: Include jQuery and Bootstrap in HTML file
+
+
+// this is the jQuery preloader.
+$(document).ready(() => {
+
+  function displayBudget() {
+    // TODO: get the remaining budget and print it to the associated <div>
+  }
+
+  function displayBreakdown() {
+    // TODO: get all of the transactions and display the overview of each
+    // item; this will separate each transaction by category and total the
+    // amount spent on each of those items.
+  }
+
+  function filterTransactions() {
+    // TODO: sort the transactions of the user by category
+  }
+
+  function swapScreen() {
+    // TODO: change between the detailed view of transactions and the
+    // overview of all transactions. 
+  }
+  
+  function showToolTips() {
+    // TODO: retrieve the tool tip data of an element and display it when
+    // a user hovers over an element.
+  }
+
+  function addTransaction() {
+    // TODO: open up a pop-up and get the category, item/service name
+    // and price spent on that item/service, then store it in an array
+  }
+
+  function printTransactionToDisplay() {
+    // TODO: print out a transaction to the detailed view <div>
+  }
+
+  function getBudget() {
+    // TODO: get the user's budget for the week and store it somewhere
+  }
+
+  /*
+  TODO: We need arrays/classes/objects for the following items:
+    
+    transaction = {
+      name,
+      category,
+      price
+    }
+
+    user = {
+      budget: value,
+      name: name,
+    }
+
+    
+
+
+   */
+
+});
