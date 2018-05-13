e5ff80f313ce0f40f66713b8ca70ff1a154284a8
AnthonyMarc23 <anthony@adaptivelite.com>
2018-05-12 21:51:22 -0400

    FUNCTIONALITY BAAAAAACK

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 "use strict";
 
-class transaction { //when data is put in pop-up box, input values assigned
-  constructor(itemCategory, itemName, itemPrice) {
-    this.itemCategory = itemCategory;
-    this.itemName = itemName;
-    this.itemPrice = itemPrice;
-  }
-
-  
-}//closing class transation block 
-
 class user {
-  constructor(transaction) {
-    this.transaction = [transaction];
-    this.budget = this.getBudget();
+  constructor() {
+    this.transactions = [];
+    this.budget = 0;
     this.filter = {
       Bills: true,
       Fashion: true,
@@ -22,6 +12,45 @@ class user {
       Savings: true
     };
     
+  }
+
+  addTransaction() {
+    let transaction = {
+      itemCategory: $("#item_category :selected").val(), //how to pull value from a dropdown
+      itemName: $("#item_name").val(), 
+      itemPrice: parseFloat($("#item_price").val())
+    };
+      $("#item_category :selected").val('');
+      $("#item_name").val('');
+      $("#item_price").val('');
+
+    this.transactions.push(transaction);
+
+
+
+    // let addHTML = (`
+    // <div class="detailedView ${currentUser.transaction.itemCategory}">
+    //   <img class="icons" src="icons/${currentUser.transaction.itemCategory}.png"></i>
+    //   <div id="">${currentUser.transaction.itemName}</div>
+    //   <div id="">${currentUser.transaction.itemPrice}</div>
+    // </div>
+    // `);
+
+    // $(".detailedViewWrapper").append(addHTML);
+    // currentUser.budget -= Number(currentUser.transaction.itemPrice);
+    this.displayBudget();
+  }
+  
+  setBudget() {
+    this.budget = parseFloat($("#weeklyBudget").val());
+    $('#enterBudgetPopUp').modal('hide');
+    this.displayBudget();
+    // TODO: get the user's budget for the week and store it somewhere
+    
+  }
+
+  getBudget() {
+    return this.transactions.reduce((value, transaction) => value - transaction.itemPrice, this.budget); 
   }
 
   buildCategories() {
@@ -43,83 +72,34 @@ class user {
     this.filter[category]=this.filter[category]?false : true;
   }
 
-  getBudget() {
-    let budget = prompt("Enter your budget for the week!"); 
-    return budget; 
-    // TODO: get the user's budget for the week and store it somewhere
-    
-  }
+
   add(info) {
     // pushing transaction info to transaction array 
     let newTransaction = new transaction(info.itemCategory, info.itemName, info.itemPrice);
     this.transaction.push(newTransaction);
+  }
+
+  displayBudget() {
+    $(".circleButton").html(`Budget:<br>$${this.getBudget()}`);
+    // TODO: get the remaining budget and print it to the associated <div>
   }
 } //closes class user block 
 
 // this is the jQuery preloader.
 $(document).ready(() => {
 
-  let currentTransaction = new transaction("Bills", "Weekly Bill", 10);
-  window.currentUser = new user(currentTransaction);
+
+  // Intialize a new user
+  window.currentUser = new user();
+  $('#enterBudgetPopUp').modal('show');
   currentUser.buildCategories();
 
-  displayBudget(currentUser.budget);
-  console.log(currentUser.budget);
-
-  //popup for add button
-  $(".add").on("click", function() {
-    console.log('hello');
-    $(".popupFormContainer").fadeIn(500); 
-  }); 
-
-  
-  $("#popUpClose").on("click", function() {
-    $(".popupFormContainer").fadeOut(500);
-  });
-
-  //popup for filter button 
-
-
-  $("#addTransactionButton").on("click", function() { 
-    currentUser.transaction = {
-          itemCategory:$("#item_category :selected").val(), //how to pull value from a dropdown
-          itemName:$("#item_name").val(), 
-          itemPrice:$("#item_price").val()
-    }
-
-    let addHTML = (`
-    <div class="detailedView ${currentUser.transaction.itemCategory}">
-      <img class="icons" src="icons/${currentUser.transaction.itemCategory}.png"></i>
-      <div id="">${currentUser.transaction.itemName}</div>
-      <div id="">${currentUser.transaction.itemPrice}</div>
-    </div>
-    `);
-
-    $(".detailedViewWrapper").append(addHTML);
-    currentUser.budget -= Number(currentUser.transaction.itemPrice);
-    displayBudget(currentUser.budget);
-
-  }); 
-
-
-
-
-  function displayBudget(budget) {
-    $(".circleButton").text(`Budget: $${budget}`);
-    // TODO: get the remaining budget and print it to the associated <div>
-  }
 
   function displayBreakdown() {
     // TODO: get all of the transactions and display the overview of each
     // item; this will separate each transaction by category and total the
     // amount spent on each of those items.
   }
-
-  $("#filterTransactionButton").on("click", function(){
-    /*
-    if class box is checked, then $("class").toggle();
-    */
-  }); 
 
   // TODO: sort the transactions of the user by category
   
@@ -131,12 +111,6 @@ $(document).ready(() => {
   }
 
 });
-
-function openFilterPopUp() {
-  $("#filterPopUp").modal("show");
-}
-
-
 
 
 
